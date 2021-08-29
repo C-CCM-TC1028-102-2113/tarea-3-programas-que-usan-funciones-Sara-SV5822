57549a56ed3897579720ff1be1c55667e891a660
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-29 12:39:30 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,19 @@
+## Año bisiesto
+def condition(year):
+    if year%4==0 and year%100==0 and year%400!=0:
+        resultado='False'
+    elif year%4!=0:
+        resultado='False'
+    elif year%4==0 and year%400==0:
+        resultado='True'
+    elif year%4==0 and year%100!=0 and year%400!=0:
+        resultado='True'
+    return resultado
 
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+  #escribe tu código abajo de esta línea
+  year=int(input(''))
+  print(condition(year))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
