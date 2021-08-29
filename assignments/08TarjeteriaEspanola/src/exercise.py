84215f146b8048e4fb90edf7b52540f4187e865b
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-29 12:23:35 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,7 +1,20 @@
+## Tarjetería Española
+def cant(papel, plumon):
 
-def main():
-    #escribe tu código abajo de esta línea
-    pass
+    plumon= plumon*35
+    papel= papel*12
+    if plumon>papel:
+        total= papel
+    elif papel>plumon:
+        total= plumon
+    return total
 
-if __name__=='__main__':
+def  main():
+  #escribe tu código abajo de esta línea
+  papel= int(input('Dame la cantidad de pliegos de papel albanene: '))
+  plumon= int(input('Dame la cantidad de plumones: '))
+
+  print('El número máximo de tarjetas que se pueden hacer es: '+ str(cant(papel,plumon)))
+
+if __name__ == '__main__':
     main()
